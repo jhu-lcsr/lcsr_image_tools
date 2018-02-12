dde6ccf030180254999eb8cfc2723b9beaa70c09
Zihan Chen <zihan.chen.jhu@gmail.com>
2018-02-11 22:48:25 -0800

    Updated to support kinetic

--- fakecam/src/video_file_publisher.cpp
+++ fakecam/src/video_file_publisher.cpp
@@ -11,7 +11,6 @@
 #include <terse_roscpp/param.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <boost/ptr_container/ptr_vector.hpp>
 
 class FakeCamera {
